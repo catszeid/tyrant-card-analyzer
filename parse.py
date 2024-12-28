fcc729ac27f97c2d2edf4c86b06d1fdcbade7a15
Keith Maitland <arctrooper1@gmail.com>
2024-12-28 11:05:57 -0700

    Fix skill string outputs, cleanup

--- parse.py
+++ parse.py
@@ -1,61 +1,78 @@
 import xml.etree.ElementTree as ET
+import os
+import re
+
 import scoring as scor
 
-# load sample xml
-tree = ET.parse('cards_section_1.xml')
-root = tree.getroot()
+def find_files(file_pattern):
+	dir_contents = os.listdir(os.getcwd())
+	# regex to match xml files
+	pattern = re.compile(file_pattern)
+	match_files = [file for file in dir_contents if pattern.fullmatch(file)]
+	return match_files
 
-# Iterate over all cards
-for card in root:
-	if card == None:
-		print("Found empty card, skipping...")
-		continue
-	card_name = card.find('name')
-	if card_name != None:
-		card_name = card_name.text
-	
-	card_cost = card.find('cost') # convert for scoring
-	if card_cost != None and card_cost.text != None:
-		card_cost = int(card_cost.text)
-	else:
-		card_cost = 0
-	
-	card_attack = card.find('attack') # convert attack and health for scoring
-	if card_attack != None and card_attack.text != None:
-		card_attack = int(card_attack.text)
-	card_health = int(card.find('health').text)
-	card_skills = card.findall('skill') # list of 'skill' Elements
-	# iterate through all upgrades for final card version
-	for upgrade in card.findall('upgrade'):
-		if upgrade.find('attack'):
-			card_attack = int(upgrade.find('attack').text)
-		if upgrade.find('health'):
-			card_health = int(upgrade.find('health').text)
-		if upgrade.find('cost'):
-			if upgrade.find('cost').text == None:
-				card_cost = 0
+def main():
+	pattern = "^cards_section_\d*.xml$"
+	files = find_files(pattern)
+	# load xml
+	for file in files:
+		tree = ET.parse(file)
+		root = tree.getroot()
+		
+		# Iterate over all cards
+		for card in root:
+			if card == None:
+				print("Found empty card, skipping...")
+				continue
+			card_name = card.find('name')
+			if card_name != None:
+				card_name = card_name.text
+			
+			card_cost = card.find('cost') # convert for scoring
+			if card_cost != None and card_cost.text != None:
+				card_cost = int(card_cost.text)
 			else:
-				card_cost = int(upgrade.find('cost').text)
-		if len(upgrade.findall('skill')) != 0:
-			# reset skills and refill the list
-			card_skills = []
-			for skill in upgrade.findall('skill'):
-				card_skills.append(skill)
-	# score stats
-	total_stats = card_health
-	if card_attack != None:
-		total_stats += card_attack
-	adjusted_stats = (total_stats) / (card_cost + 1)
-	# score skills
-	skill_score = 0
-	avg_skill_score = 0.0
-	# convert skills from Element to readable string
-	final_skills = []
-	for skill in card_skills:
-		skill_score += scor.score_skill(skill)
-		final_skills.append(scor.skill_to_string(skill))
-	if len(final_skills) > 0:
-		avg_skill_score = skill_score / len(final_skills)
-	card_result = "{} [{},{:.5}]({}) - [{:.5}]"
-	card_result += " {}"*len(card_skills)
-	print(card_result.format(card_name, total_stats, adjusted_stats, card_cost, avg_skill_score, *final_skills))
+				card_cost = 0
+			
+			card_attack = card.find('attack') # convert attack and health for scoring
+			if card_attack != None and card_attack.text != None:
+				card_attack = int(card_attack.text)
+			card_health = int(card.find('health').text)
+			card_skills = card.findall('skill') # list of 'skill' Elements
+			# iterate through all upgrades for final card version
+			for upgrade in card.findall('upgrade'):
+				if upgrade.find('attack'):
+					card_attack = int(upgrade.find('attack').text)
+				if upgrade.find('health'):
+					card_health = int(upgrade.find('health').text)
+				if upgrade.find('cost'):
+					if upgrade.find('cost').text == None:
+						card_cost = 0
+					else:
+						card_cost = int(upgrade.find('cost').text)
+				if len(upgrade.findall('skill')) != 0:
+					# reset skills and refill the list
+					card_skills = []
+					for skill in upgrade.findall('skill'):
+						card_skills.append(skill)
+			# score stats
+			total_stats = card_health
+			if card_attack != None:
+				total_stats += card_attack
+			adjusted_stats = (total_stats) / (card_cost + 1)
+			# score skills
+			skill_score = 0
+			avg_skill_score = 0.0
+			# convert skills from Element to readable string
+			final_skills = []
+			for skill in card_skills:
+				skill_score += scor.score_skill(skill)
+				final_skills.append(scor.skill_to_string(skill))
+			if len(final_skills) > 0:
+				avg_skill_score = skill_score / len(final_skills)
+			card_result = "{} [{},{:.5}]({}) - [{:.5}]"
+			card_result += " {}"*len(card_skills)
+			print(card_result.format(card_name, total_stats, adjusted_stats, card_cost, avg_skill_score, *final_skills))
+
+if __name__ == "__main__":
+	main()
--- scoring.py
+++ scoring.py
@@ -3,22 +3,27 @@ from vars import SKILLS, TYPES
 # Scoring functions to process card values
 
 # Score a skill's budget
-def score_skill(skill):
+def score_skill(skill) -> int:
 	id = skill.get('id')
 	score = 0
 	if id == 'jam' or id == 'flurry': # complex pricing, see below
-		s_c = int(skill.get('c'))
 		s_n = skill.get('n')
 		if s_n: # convert to int for later calculations
 			s_n = int(s_n)
 		else:
 			s_n = 1
-		cooldown_decrease = 8-s_c
-		score += 10 # estimated 10 for flurry 1 every 8
-		# estimated 10 additional per reduced cooldown (c)
-		score += cooldown_decrease*10
-		# estimated 10 additional per iteration (n)
-		score += (s_n - 1) * 10
+		# trigger + count "On trigger: Jam n"
+		if skill.get('trigger'):
+			score += 30 * s_n
+		else: # "Jam/Flurry n every c"
+			if skill.get('c'):
+				s_c = int(skill.get('c'))
+			cooldown_decrease = 8-s_c
+			score += 10 # estimated 10 for flurry 1 every 8
+			# estimated 10 additional per reduced cooldown (c)
+			score += cooldown_decrease*10
+			# estimated 10 additional per iteration (n)
+			score += (s_n - 1) * 10
 	elif id == 'allegiance' or id == 'legion' or id == 'coalition' or id == 'scavenge':
 		score = int(skill.get('x'))*2 # 2 budget per skill
 	elif id == 'revenge' or id == 'inhibt' or id == 'evade':
@@ -28,43 +33,96 @@ def score_skill(skill):
 	elif id == 'wall':
 		score = 10
 	elif id == 'overload':
-		score = int(skill.get('n'))*15 # 15 budger per skill
+		if skill.get('n'):
+			score = int(skill.get('n'))*15 # 15 budget per skill
+		else:
+			score += 15
+	elif id == 'summon':
+		score = 10 # todo calculate summon value
+	elif id == 'flying':
+		score = 69 # nice
+	elif id == 'evolve': # n, s (base skill), s2 (upgrade)
+		s_n = skill.get('n')
+		if s_n:
+			s_n = int(s_n)
+		else:
+			s_n = 1
+		s_s = skill.get('s')
+		s_s2 = skill.get('s2')
+		score += s_n * 30
+	elif id == 'enhance': # x, s (skill)
+		s_x = skill.get('x')
+		if s_x:
+			s_x = int(s_x)
+		else:
+			s_x = 1
+		score += s_x * 15 # check rate
 	else: # default 1:1 budgeting
 		score = skill.get('x')
-		if score:
-			score = int(score)
-	return score
+	return int(score)
 
 # Convert skill Element to string for display
 def skill_to_string(skill) -> str:
-	out = "" # string for output
+	out = ""
 	id = skill.get("id")
 	if skill.get("trigger"): # trigger is prefix
 		out += "On " + skill.get("trigger") + ": "
-	out += id # skill name
+	s_all = skill.get('all')
 
 	if id == 'jam' or id == 'flurry': # use 'n' and 'c' only. Iterations and Cooldown respectively
+		out += id
 		s_n = skill.get('n')
 		if s_n == None:
 			s_n = 1
-		s_c = skill.get('c')
-		out += " {} every {}".format(s_n, s_c)
-		return out
+		if skill.get('trigger'):
+			out += f" {s_n}"
+		else:
+			s_c = skill.get('c')
+			out += " {} every {}".format(s_n, s_c)
 	elif id == 'summon': # use 'card_id' only. Refers to the card summoned
+		out += id
 		s_card_id = skill.get('card_id')
-		return out + f'Summon {s_card_id}'
+		out += f' {s_card_id}'
 	elif id == 'overload':
+		out += id
+		s_y = skill.get('y')
+		if s_y:
+			out += f" {TYPES[int(skill.get('y'))]}"
 		s_n = skill.get('n')
 		if s_n == None:
 			s_n = 1
 		out += " {}".format(s_n)
-		return out
+
+	elif id == 'enhance':
+		out += id
+		if s_all:
+			out += " All"
+		# check if n is ever used
+		s_x = skill.get('x')
+		s_s = skill.get('s')
+		out += f" {s_s} {s_x}"
+	elif id == 'evolve':
+		out += id
+		s_s = skill.get('s')
+		s_s2 = skill.get('s2')
+		if s_all:
+			out += " All"
+		else:
+			s_n = skill.get('n')
+			if s_n == None:
+				s_n = 1
+			out += f" {s_n}"
+
+		out += f" {s_s} to {s_s2}"
 	elif id == 'wall': # No addition parameters
-		return "Wall"
+		out += id
+	elif id == 'flying':
+		out += id
 	else: # standard skill nomenclature
+		out += id
 		if skill.get('all'): # affect all elegible targets
-			out.join(" all")
+			out += " all"
 		if skill.get('y'): # faction restriction
 			out += f" {TYPES[int(skill.get('y'))]}"
-		out += " " + skill.get('x') #power
-		return out
+		out += f" {skill.get('x')}" # power
+	return out
